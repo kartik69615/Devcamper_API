e8d5ec16cccc84523cb26d07db2cea793c005636
kartik69615 <kartikkumar69615.com>
2025-07-22 09:03:39 +0530

    connect with mongoDB

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger'); 
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load environment variables from config.env
+dotenv.config({ path: './config/config.env' });
+
+// Connect to the database
+connectDB();
 
 
 // Route files
 const bootcamps = require('./router/bootcamps');
 
-// Load environment variables from config.env
-dotenv.config({ path: './config/config.env' });
-
 const app = express();
-
 
 
 // Dev logging middleware
@@ -21,13 +25,19 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 
-
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
